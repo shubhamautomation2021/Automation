302767316149194df6293c5f34edb69df70d413e
shubhamautomation2021 <90815507+shubhamautomation2021@users.noreply.github.com>
2021-09-17 09:24:31 +0530

    Update Demo.java

--- AutomationQSP/src/QSP/Demo.java
+++ AutomationQSP/src/QSP/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Happy Ganesh Chaturthi");
+		System.out.println("Happy Ganesh Chaturthi/ Vishwakarma Puja");
 	}
 }
