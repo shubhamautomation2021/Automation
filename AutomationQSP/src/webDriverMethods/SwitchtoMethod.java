83c02cfd49fdd46ade65bc6f5246dae7df2971f2
SHUBHAM <SHUBHAM@Shubham>
2021-09-25 09:29:39 +0530

    Method

---
+++ AutomationQSP/src/webDriverMethods/SwitchtoMethod.java
@@ -0,0 +1,25 @@
+package webDriverMethods;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwitchtoMethod {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.navigate().to("https://www.google.com");
+		driver.switchTo().activeElement().sendKeys("Java",Keys.ENTER);
+		Thread.sleep(2000);
+		Navigation nav = driver.navigate();
+		nav.back();
+		Thread.sleep(2000);
+		nav.forward();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		
+	}
+
+}
+
