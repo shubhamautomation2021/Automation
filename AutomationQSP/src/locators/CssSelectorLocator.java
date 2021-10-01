30517bc0e096fdf7027ae4b89fdc5a6849e7fd9a
SHUBHAM <SHUBHAM@Shubham>
2021-10-01 12:02:28 +0530

    Xpath

---
+++ AutomationQSP/src/locators/CssSelectorLocator.java
@@ -0,0 +1,19 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CssSelectorLocator {
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");                                                                                            
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[title^='Sea']")).sendKeys("automation");
+	}
+
+}
---
+++ AutomationQSP/src/xpathLocator/XpathUsingUniqueAttribute.java
@@ -0,0 +1,24 @@
+package xpathLocator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathUsingUniqueAttribute {
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");                                                                                            
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input [@id='txtUsername']")).sendKeys("Admin");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+	}
+}
+
